7fa6ef19db70079fa51f7489c093c5183683ba05
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-05-30 11:30:47 +0900

    [Passed] 배수의 개수 (140ms, 10MB)

---
+++ trail0/배수의 개수/number-of-multipliers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(int i=0; i<10; i++){
+            int a = sc.nextInt();
+            if(a % 3 == 0){
+                cnt1 += 1;
+            }
+            if(a % 5 == 0){
+                cnt2 += 1;
+            }
+        }
+        System.out.printf("%d %d", cnt1, cnt2);
+    }
+}
